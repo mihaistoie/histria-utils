e1d8abf813da77a8a4c1273151e23992adcc98f3
misto <mihaistoie@gmail.com>
2017-01-28 13:25:25 +0100

    rename storage to store

---
+++ src/lib/interfaces/store.ts
@@ -0,0 +1,5 @@
+export interface IStore {
+    findOne(entityName: string, filter: any, options: any): Promise<any>;
+    find(entityName: string, filter: any, options: any):  Promise<any[]>;
+    initNameSpace(nameSpace: string, model: any, data: any[]):  Promise<void>;
+}
