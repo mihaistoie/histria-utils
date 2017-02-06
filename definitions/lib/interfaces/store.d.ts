27f124586d0da44fafc842e298f272663815dbec
Mihai  STOIE <mihaistoie@gmail.com>
2017-02-06 14:19:19 +0100

    update store interface

--- definitions/lib/interfaces/store.d.ts
+++ definitions/lib/interfaces/store.d.ts
@@ -1,5 +1,9 @@
 export interface IStore {
-    findOne(entityName: string, filter: any, options: any): Promise<any>;
-    find(entityName: string, filter: any, options: any): Promise<any[]>;
+    findOne(entityName: string, filter: any, options?: {
+        compositions: boolean;
+    }): Promise<any>;
+    find(entityName: string, filter: any, options?: {
+        compositions: boolean;
+    }): Promise<any[]>;
     initNameSpace(nameSpace: string, data: any[]): Promise<void>;
 }
