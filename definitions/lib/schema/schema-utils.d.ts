c6bed87f0c7d4d8ee2e49bf9ada485f78df310dd
Mihai  STOIE <mihaistoie@gmail.com>
2017-04-12 13:38:08 +0200

    Schema manager :  childrenAndRefsOfClass bugs fixed

--- definitions/lib/schema/schema-utils.d.ts
+++ definitions/lib/schema/schema-utils.d.ts
@@ -16,5 +16,4 @@ export declare function getChildrenAndRefsOfClass(schema: any, mapper: (fullClas
     refs: string[];
 };
 export declare function isChild(schema: any): boolean;
-export declare function isTree(schema: any): boolean;
 export declare function loadModel(pathToModel: string, model: any): Promise<void>;
