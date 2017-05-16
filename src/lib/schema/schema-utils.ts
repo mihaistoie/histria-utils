352c0d40ce2b92b8d68959f0f7b39eb1ee7060cc
Mihai  STOIE <mihaistoie@gmail.com>
2017-05-16 15:38:58 +0200

    checkModel improved

--- src/lib/schema/schema-utils.ts
+++ src/lib/schema/schema-utils.ts
@@ -388,8 +388,8 @@ function _checkRelations(schema: any, model: any) {
             if (rel.aggregationKind === AGGREGATION_KIND.shared) {
                 throw util.format('Invalid relation "%s.%s", a view can\'t have aggregations.', schema.name, relName);
             }
-            if (rel.aggregationKind === AGGREGATION_KIND.composite) {
-                if (!refModel)
+            if (rel.type === RELATION_TYPE.hasOne && rel.aggregationKind === AGGREGATION_KIND.composite) {
+                if (refModel.view && !refRel)
                     throw util.format('Invalid relation "%s.%s": "%s" invRel is empty.', schema.name, relName, rel.model);
             }
             let lf = relName + 'Id';
