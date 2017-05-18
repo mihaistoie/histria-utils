8c7ebba50df932a9a3d6e31fdde82de362305df8
Mihai  STOIE <mihaistoie@gmail.com>
2017-05-18 10:28:14 +0200

    check relations bug fixed

--- src/lib/schema/schema-utils.ts
+++ src/lib/schema/schema-utils.ts
@@ -400,7 +400,7 @@ function _checkRelations(schema: any, model: any) {
                 rel.foreignFields = ['id'];
                 schema.properties[lf] = refIdDefinition();
             } else if (rel.type === RELATION_TYPE.hasMany) {
-                if (refRel.view) {
+                if (refRel && refRel.view) {
                     rel.localFields = ['id'];
                     rel.foreignFields = [rel.invRel + 'Id'];
                 } else {
