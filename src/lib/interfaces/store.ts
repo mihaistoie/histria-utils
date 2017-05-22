21fc1c1d38d82c68f8e4277bd9f7e68717887288
Mihai  STOIE <mihaistoie@gmail.com>
2017-05-22 14:22:15 +0200

    IStore - method save added

--- src/lib/interfaces/store.ts
+++ src/lib/interfaces/store.ts
@@ -1,5 +1,6 @@
 export interface IStore {
     findOne(entityName: string, filter: any, options?: { compositions: boolean }): Promise<any>;
     find(entityName: string, filter: any, options?: { compositions: boolean }): Promise<any[]>;
+    save(data: any): Promise<void>;
     initNameSpace(nameSpace: string, data: any): Promise<void>;
 }
